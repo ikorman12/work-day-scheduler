3ce74743fd620355f0fccff01caa2f287e6c7b6f
Igor Korman <ikorman12@gmail.com>
2022-11-05 15:46:47 -0400

    added save button to porgram

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 // ASSIGNMENT CODE
 var currentHour = new Date().getHours();
+var activity= this.siblings('.description').val().localStorage.setItem(currentHour);
 function init() {
         for (var i = 9; i < 18; i++) {
         var colorKey = "";
@@ -20,11 +21,10 @@ function init() {
             }   
     var row = $("<div>").addClass("row time-block");
     var hour = $("<div>").addClass("col-2 hour").text(hourDisplay);
-    var textArea = $("<textarea>")
-      .addClass("col-8 description " + colorKey)
-      .val(localStorage.getItem(i)); 
-      
-    $(".container").append(row.append(hour, textArea));
+    var textArea = $("<textarea>").addClass("col-8 description " + colorKey).val(localStorage.getItem(i));$(".container").append(row.append(hour, textArea));
+    var saveBtn = $(`<button>`).addClass('col-2 saveBtn').click(function(){ 
+        localStorage.setItem(currentHour, activity);
+    })
   };
 
 // INITIALIZATION
